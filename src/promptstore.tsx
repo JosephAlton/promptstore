2c1abdb81cd765fbf7efd413e1d361c0dbd9dc35
Joseph Alton <45772406+JosephAlton@users.noreply.github.com>
2025-09-06 16:42:20 +0100

    Dev (#3)
    * toggle for default cut/copy done... strange issue where LocalStorage was not using booleans but numbers, so just switched to storing numbers
    
    * comment for example Add prompt action
    
    * planning and restructing to be able to multiple prompts
    
    * Multi prompts (#2)
    
    * setting up to use multiple prompts
    
    * multiple prompts being stored and switching between seems to work... still missing the first value though, and not exluding the selected as it should be
    
    * switching prompts works well
    
    * mvp for multi prompt finished
    
    * better keyboard shortcuts
    
    * plans for the discarding behavoir
    
    * shortcuts update

--- src/promptstore.tsx
+++ src/promptstore.tsx
@@ -1,9 +1,24 @@
-import { Form, Detail, LocalStorage, Action, ActionPanel, Clipboard, Icon, popToRoot, showToast, Toast } from "@raycast/api";
+import { Form, Detail, LocalStorage, Action, ActionPanel, Clipboard, Icon, popToRoot, showToast, Toast, KeyEquivalent } from "@raycast/api";
 import { useEffect, useState } from "react";
+
+const DEFAULT_ACTION_KEY = "defaultAction";
+const PROMPT_KEY = "prompt";
+const PROMPT_SELECTED_KEY = "promptSelected";
+const PROMPTS_COUNT_KEY = "promptsCount";
+
+
 
 export default function Command() {
     const [prompt, setPrompt] = useState<string | null>(null);
     const [cutAsDefault, setCutAsDefault] = useState<boolean>(true);
+
+
+    const [promptsCount, setPromptsCount] = useState<number>(1);
+    const [promptSelected, setPromptSelected] = useState<number>(0);
+
+
+
+
 
 
     async function copiedToastAndClose() {
@@ -18,13 +33,36 @@ export default function Command() {
 
     useEffect(() => {
         async function load() {
-            const savedPrompt = await LocalStorage.getItem("prompt");
-            setPrompt(typeof savedPrompt === "string" ? savedPrompt : "");
+            let promptSelectedAux = await LocalStorage.getItem(PROMPT_SELECTED_KEY);
+            promptSelectedAux = typeof promptSelectedAux === "number" ? promptSelectedAux : 0;
 
-            const savedCutAsDefault = await LocalStorage.getItem("defaultAction");
 
-            if (typeof savedCutAsDefault === "boolean") {
-                setCutAsDefault(savedCutAsDefault);
+            setPromptSelected(promptSelectedAux);
+
+
+            let promptsCountAux = await LocalStorage.getItem(PROMPTS_COUNT_KEY);
+            promptsCountAux = typeof promptsCountAux === "number" ? promptsCountAux : 1;
+            setPromptsCount(promptsCountAux);
+
+
+            let savedPrompt = await LocalStorage.getItem(PROMPT_KEY + promptSelectedAux);
+            savedPrompt = typeof savedPrompt === "string" ? savedPrompt : "";
+
+            setPrompt(savedPrompt);
+
+
+            const savedCutAsDefault = await LocalStorage.getItem(DEFAULT_ACTION_KEY);
+
+            switch (savedCutAsDefault) {
+                case 1:
+                    setCutAsDefault(true);
+                    break;
+                case 0:
+                    setCutAsDefault(false);
+                    break;
+                default:
+                    await LocalStorage.setItem(DEFAULT_ACTION_KEY, 1);
+                    break;
             }
         }
         load();
@@ -34,49 +72,149 @@ export default function Command() {
         return <Detail markdown="Loading..." />;
     }
 
+
+
+    async function copy() {
+        await Clipboard.copy(prompt ?? "");
+        await copiedToastAndClose();
+    }
+
+    async function cut() {
+        await Clipboard.copy(prompt ?? "");
+        await LocalStorage.removeItem(PROMPT_KEY + promptSelected);
+        await copiedToastAndClose();
+        // more work
+    }
+
+
+    async function switchPrompt(index: number) {
+        await LocalStorage.setItem(PROMPT_SELECTED_KEY, index);
+        const promptAux = await LocalStorage.getItem(PROMPT_KEY + index);
+        setPrompt(typeof promptAux === "string" ? promptAux : "");
+        setPromptSelected(index);
+    }
+
+
+
     return (
         <Form
             actions={
                 <ActionPanel>
-                    <Action.CopyToClipboard
-                        title="Cut to clipboard"
+                    <Action
+                        title={cutAsDefault ? "Cut to clipboard" : "Copy to clipboard"}
                         icon={Icon.Clipboard}
                         shortcut={{ modifiers: ["cmd"], key: "return" }}
-                        onCopy={async () => {
-                            await LocalStorage.removeItem("prompt");
-                            await copiedToastAndClose();
-                        }}
-                        content={prompt}
-                    />
-
-
-                    <Action
-                        title="Copy to clipboard"
-                        icon={Icon.Duplicate}
-                        shortcut={{ modifiers: ["cmd"], key: "c" }}
                         onAction={async () => {
-                            await Clipboard.copy(prompt);
-                            await copiedToastAndClose();
+                            cutAsDefault ? await cut() : await copy();
                         }}
                     />
 
 
+                    {
+                        prompt !== "" && (
+
+                            <Action
+                                title="Add"
+                                icon={Icon.Plus}
+                                shortcut={{ modifiers: ["cmd"], key: "=" }}
+                                onAction={async () => {
+                                    await switchPrompt(promptsCount);
+                                    await LocalStorage.setItem(PROMPTS_COUNT_KEY, promptsCount + 1);
+                                    setPromptsCount(promptsCount + 1);
+
+                                }}
+                            />
+                        )}
+
+                    {
+                        promptSelected > 0 && (
+                            <Action
+                                title="Previous"
+                                icon={Icon.ArrowLeft}
+                                shortcut={{ modifiers: ["ctrl"], key: "," }}
+                                onAction={async () => {
+                                    await switchPrompt(promptSelected - 1);
+                                }}
+                            />
+                        )
+                    }
+
+                    {
+                        Array.from({ length: promptsCount }, (_, index) => {
+                            // Skip the currently selected prompt
+                            if (index === promptSelected) {
+                                return null;
+                            }
+
+                            // Only show actions up to index 8 (prompt 9)
+                            if (index > 8) {
+                                return null;
+                            }
+
+                            return <Action
+                                key={`prompt-action-${index}`}
+                                title={(index + 1).toString()}
+                                icon={Icon.Switch}
+                                shortcut={{ modifiers: ["cmd"], key: (index + 1).toString() as KeyEquivalent }}
+                                onAction={async () => {
+                                    await switchPrompt(index);
+                                }}
+                            />
+                        }).filter(Boolean)
+                    }
+
+
+                    {
+                        promptSelected < promptsCount - 1 && (
+                            <Action
+                                title="Next"
+                                icon={Icon.ArrowRight}
+                                shortcut={{ modifiers: ["ctrl"], key: "." }}
+                                onAction={async () => {
+                                    await switchPrompt(promptSelected + 1);
+                                }}
+                            />
+                        )
+                    }
+
+
+
                     <Action
+                        title={cutAsDefault ? "Copy to clipboard" : "Cut to clipboard"}
+                        icon={Icon.Duplicate}
+                        shortcut={{ modifiers: ["cmd"], key: cutAsDefault ? "c" : "x" }}
+                        onAction={async () => {
+                            cutAsDefault ? await copy() : await cut();
+                        }}
+                    />
+
+                    {
+                        promptsCount > 1 && (
+                            <Action
+                                title="Delete"
+                                icon={Icon.Trash}
+                                shortcut={{ modifiers: ["cmd"], key: "-" }}
+                            />
+                        )
+                    }
+
+
+                    {/* <Action
                         title="Clear"
                         icon={Icon.Trash}
-                        shortcut={{ modifiers: ["cmd"], key: "backspace" }}
+                        shortcut={{ modifiers: ["cmd"], key: "-" }}
                         onAction={async () => {
                             setPrompt("");
-                            await LocalStorage.removeItem("prompt");
+                            await LocalStorage.removeItem(PROMPT_KEY + promptSelected);
                         }}
-                    />
+                    /> */}
 
                     <Action
                         title={`${cutAsDefault ? "COPY" : "CUT"} as default`}
                         icon={Icon.Gear}
                         onAction={async () => {
                             setCutAsDefault(!cutAsDefault);
-                            await LocalStorage.setItem("defaultAction", cutAsDefault ? "COPY" : "CUT");
+                            await LocalStorage.setItem(DEFAULT_ACTION_KEY, !cutAsDefault);
                         }}
                     />
 
@@ -85,15 +223,23 @@ export default function Command() {
                 </ActionPanel>
             }
         >
+            {
+                promptsCount > 1 && (
+                    <Form.Description text={`${promptSelected + 1} of ${promptsCount}`} />
+                )
+            }
+
             <Form.TextArea
-                id="prompt"
+                id={"activePrompt"}
                 value={prompt}
                 onChange={async (value) => {
                     setPrompt(value);
-                    await LocalStorage.setItem("prompt", value);
+                    await LocalStorage.setItem(PROMPT_KEY + promptSelected, value);
                 }}
             />
             <Form.Description text={`characters: ${prompt?.length}`} />
+
+
         </Form>
     );
 }
